d9ba28871b8d822a0acbfd9b4ececf8c8f450aeb
Nobinator <nobinator.root@gmail.com>
2017-07-23 18:21:33 +0300

    Effectors

---
+++ Assets/Effectors/GizmosDrawer.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Effectors{
+	public class GizmosDrawer : MonoBehaviour{
+		
+		
+		
+		public AreaEffector2D area;
+
+		void OnDrawGizmos() {
+			Gizmos.color = Color.red;
+			var direction = transform.TransformDirection(Quaternion.Euler(0,0,area.forceAngle)*Vector3.right) * 2;
+			Gizmos.DrawRay(transform.position, direction);
+		}
+	}
+}
