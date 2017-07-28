bf51143c28cc784fe870315b966377fa5d58a6e1
Nobinator <nobinator.root@gmail.com>
2017-07-28 16:32:19 +0300

    TrailRenderer

---
+++ Assets/LineAndTrailRenderers/MovableObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableObject : MonoBehaviour{
+
+	public const float movingSpeed = 0.05f;
+	
+	void Update () {
+		if(Input.GetKey(KeyCode.A)){
+			transform.Translate(Vector3.left * movingSpeed);
+		}
+		if(Input.GetKey(KeyCode.D)){
+			transform.Translate(Vector3.right * movingSpeed);
+		}
+		if(Input.GetKey(KeyCode.W)){
+			transform.Translate(Vector3.forward * movingSpeed);
+		}
+		if(Input.GetKey(KeyCode.S)){
+			transform.Translate(Vector3.back * movingSpeed);
+		}
+	}
+}
