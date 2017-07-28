49ed8c39c4cf31e327b2aa40b3c16338f2a155ef
Nobinator <nobinator.root@gmail.com>
2017-07-28 16:43:15 +0300

    LineRenderer

---
+++ Assets/LineAndTrailRenderers/Line.cs
@@ -0,0 +1,29 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Line : MonoBehaviour {
+
+	public LineRenderer lineRenderer;
+
+	List<Vector3> points;
+
+	public void UpdateLine (Vector3 point){
+		if (points == null){
+			points = new List<Vector3>();
+			SetPoint(point);
+			return;
+		}
+
+		if (Vector3.Distance(points.Last(), point) > .1f)
+			SetPoint(point);
+	}
+
+	void SetPoint (Vector3 point){
+		points.Add(point);
+
+		lineRenderer.positionCount = points.Count;
+		lineRenderer.SetPosition(points.Count - 1, point);
+	}
+
+}
---
+++ Assets/LineAndTrailRenderers/Pen.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pen : MonoBehaviour {
+
+	public GameObject linePrefab;
+
+	Line activeLine;
+	
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.Q)){
+			if(activeLine == null){
+				var go = Instantiate(linePrefab);
+				activeLine = go.GetComponent<Line>();
+			} else {
+				activeLine = null;
+			}
+		}
+		
+		if (activeLine != null){
+			activeLine.UpdateLine(transform.position);
+		}
+	}
+}
