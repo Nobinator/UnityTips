5f67a190be38c2957277502954c1f63596edfe9b
Nobinator <nobinator.root@gmail.com>
2017-07-29 20:30:29 +0300

    MovableObject as common object

--- Assets/MovableObject.cs
+++ Assets/MovableObject.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class MovableObject : MonoBehaviour{
 
-	public const float movingSpeed = 0.05f;
+	public float movingSpeed = 0.05f;
+	public float angularSpeed = 2f;
+
+	public bool angularRule = true;
 	
 	void Update () {
 		if(Input.GetKey(KeyCode.A)){
-			transform.Translate(Vector3.left * movingSpeed);
+			if(angularRule)
+				transform.Rotate(Vector3.up,-angularSpeed);
+			else
+				transform.Translate(Vector3.left * movingSpeed);
 		}
 		if(Input.GetKey(KeyCode.D)){
-			transform.Translate(Vector3.right * movingSpeed);
+			if(angularRule)
+				transform.Rotate(Vector3.up,angularSpeed);
+			else
+				transform.Translate(Vector3.right * movingSpeed);
 		}
 		if(Input.GetKey(KeyCode.W)){
 			transform.Translate(Vector3.forward * movingSpeed);
