4714ee58f99dadbd70066f3e6b89ffafc34c7d9e
Nobinator <nobinator.root@gmail.com>
2017-07-29 20:32:40 +0300

    Perlin Noise things (not final tip)

---
+++ Assets/PerlinNoise/MapDisplay.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapDisplay : MonoBehaviour{
+
+	public Renderer textureRenderer;
+
+	public void Draw(float[,] noiseMap){
+		int width = noiseMap.GetLength(0);
+		int height = noiseMap.GetLength(1);
+		
+		Texture2D tex = new Texture2D(width,height);
+
+		Color[] colorMap = new Color[width * height];
+		for (int y = 0; y < height; y++){
+			for (int x = 0; x < width; x++){
+				colorMap[y * width + x] = Color.Lerp(Color.black, Color.white, noiseMap[x, y]);
+			}
+		}
+		
+		tex.SetPixels(colorMap);
+		tex.Apply();
+
+		textureRenderer.sharedMaterial.mainTexture = tex;
+		textureRenderer.transform.localScale = new Vector3(width,1,height);
+	}
+
+}
---
+++ Assets/PerlinNoise/MapGeneratorEditor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor {
+
+	public override void OnInspectorGUI(){
+		MapGenerator m = (MapGenerator) target;
+
+		// Если какое-то из значений было изменено
+		if(DrawDefaultInspector()){
+			if(m.autoUpdate)
+				m.GenerateMap();
+		}
+
+		if(GUILayout.Button("Generate")){
+			m.GenerateMap();
+		}
+	}
+
+}
