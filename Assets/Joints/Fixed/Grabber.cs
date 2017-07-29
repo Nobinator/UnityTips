152dc38d94fdbb27629adcbaaff0037fcb941b67
Nobinator <nobinator.root@gmail.com>
2017-07-29 20:31:55 +0300

    FixedJoind and almost SpringJoint

---
+++ Assets/Joints/Fixed/Grabber.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grabber : MonoBehaviour{
+
+	public FixedJoint fj;
+	public bool isGrabbing;
+	public Material m;
+
+	void Start(){
+		m.color = isGrabbing ? Color.yellow : Color.gray;
+	}
+
+	private void OnTriggerEnter(Collider other){
+		if(!isGrabbing) return;
+		
+		var r = other.gameObject.GetComponent<Rigidbody>();
+		if(r == null) return;
+		
+		fj.connectedBody = r;
+		m.color = Color.green;
+	}
+	
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.F)){
+			if(isGrabbing){
+				isGrabbing = false;
+				fj.connectedBody = null;
+				m.color = Color.gray;
+			}
+			else{
+				isGrabbing = true;
+				m.color = Color.yellow;
+			}
+		}
+		}
+}
---
+++ Assets/Joints/Spring/SpringDrawer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpringDrawer : MonoBehaviour{
+	private SpringJoint[] sjs;
+
+	void Start(){
+		sjs = GetComponents<SpringJoint>();
+	}
+
+	void Update(){
+		foreach(var sj in sjs){
+			Debug.DrawLine(sj.anchor,sj.connectedAnchor,Color.cyan);
+		}
+	}
+}
