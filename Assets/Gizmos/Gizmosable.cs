5b7609fb1943ccc72277ee878bbd6113c387bd5f
Nobinator <nobinator.root@gmail.com>
2017-07-31 14:15:41 +0300

    Gizmos tip

---
+++ Assets/Gizmos/Gizmosable.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gizmosable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// Рисует всегда
+	private void OnDrawGizmos(){
+		Gizmos.color = Color.magenta *0.2f;
+		Gizmos.DrawCube(transform.position,transform.localScale);
+	}
+
+	// Рисует только при выделении
+	private void OnDrawGizmosSelected(){
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireCube(transform.position,transform.localScale);
+	}
+}
