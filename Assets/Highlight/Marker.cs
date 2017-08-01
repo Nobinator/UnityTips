1111c2b173758fcf1cbe6b3e78570a57bf97352e
Nobinator <nobinator.root@gmail.com>
2017-08-01 17:30:49 +0300

    Highlight tip

---
+++ Assets/Highlight/Marker.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Marker : MonoBehaviour {
+
+	[MenuItem("Window/Nobi/Marker/Do highlight")]
+	public static void Do(){
+		Highlighter.Highlight("Inspector","Scale");
+	}
+	
+	[MenuItem("Window/Nobi/Marker/Stop highlight")]
+	public static void Stop(){
+		Highlighter.Stop();
+	}
+}
