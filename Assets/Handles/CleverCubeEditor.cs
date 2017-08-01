b78e00cc5759e9508ac8f74ec2a5c9ae816274f5
Nobinator <nobinator.root@gmail.com>
2017-08-01 13:24:52 +0300

    Disabling default handles and Handles.FreeMoveHandle(...)

--- Assets/Handles/CleverCubeEditor.cs
+++ Assets/Handles/CleverCubeEditor.cs
@@ -7,115 +7,129 @@ public class CleverCubeEditor : Editor{
 	
 	// Переменные будут сбиваться постоянно после того, как будет убрано выделение
 	private bool forwardArrow = false;
-	private int handler = 9;
-	private int maxhandler = 14;
+	private int handler = 12;
+	private int maxhandler = 20;
 	
 	
 	private CleverCube clv;
 	
+	Tool lastTool = Tool.None;
+
+	private void OnEnable(){
+		// При включении текущего хандлера убираем стандартный
+		lastTool = Tools.current;
+		Tools.current = Tool.None;
+	}
+
+	private void OnDisable(){
+		// Возвращаем прежний хандлер после выхода из текущего хандлера
+		Tools.current = lastTool;
+	}
+
 	protected virtual void OnSceneGUI(){
 		clv = (CleverCube) target;
-		if (clv == null)
+		if(clv == null)
 			return;
 
 		var t = clv.transform;
-		
+
 		// Handles.BeginGUI
 		// Handles.EndGUI
 		/* Можно рисовать GUI прямо поверх сцены */
 		Handles.BeginGUI();
-		
-			//if (GUILayout.Button("Forward Arrow", GUILayout.Width(100))){
-			//	forwardArrow = true;
-				// Вызов Handles не GUI внутри блока BeginGUI - EndGUI не имеет смысла
-			//}
-		
-			if (GUILayout.Button("Handler "+handler, GUILayout.Width(100))){
-				handler++;
-				if(handler >= maxhandler) handler = 0;
-			}
-		
+
+		//if (GUILayout.Button("Forward Arrow", GUILayout.Width(100))){
+		//	forwardArrow = true;
+		// Вызов Handles не GUI внутри блока BeginGUI - EndGUI не имеет смысла
+		//}
+
+		if(GUILayout.Button("Handler " + handler, GUILayout.Width(100))){
+			handler++;
+			if(handler >= maxhandler) handler = 0;
+		}
+
 		Handles.EndGUI();
 
-		
+
 		if(handler == 0){
 			// Стрелка с палочкой
 			Handles.ArrowHandleCap(0, t.position + t.forward, t.rotation, .2f, EventType.Repaint);
-			
-		}else if(handler == 1){
-			
+
+		} else if(handler == 1){
+
 			// Handles.Button (Make a 3D Button)
-			if (Handles.Button(t.position + Vector3.up*1.3f, t.rotation, .3f, .2f, Handles.RectangleHandleCap))
+			if(Handles.Button(t.position + Vector3.up * 1.3f, t.rotation, .3f, .2f, Handles.RectangleHandleCap))
 				Debug.Log("The button was pressed!");
-			
-		}else if(handler == 2){
+
+		} else if(handler == 2){
 			// Окружность
 			Handles.color = Handles.zAxisColor;
 			Handles.CircleHandleCap(
 				0,
-				t.position + Vector3.forward*1.2f,
+				t.position + Vector3.forward * 1.2f,
 				t.rotation * Quaternion.LookRotation(Vector3.forward),
-				t.localScale.z*.5f,// под size, вероятно, подразумевается радиус, ибо всегда в два раза больше назначеррного расстояния.
+				t.localScale.z *
+				.5f, // под size, вероятно, подразумевается радиус, ибо всегда в два раза больше назначеррного расстояния.
 				EventType.Repaint
 			);
-			
-		}else if(handler == 3){
+
+		} else if(handler == 3){
 			// Конус (ArrowHead)
 			Handles.color = Handles.zAxisColor;
 			Handles.ConeHandleCap(
 				0,
-				t.position + Vector3.forward*1.2f,
+				t.position + Vector3.forward * 1.2f,
 				t.rotation * Quaternion.LookRotation(Vector3.forward),
 				0.3f,
 				EventType.Repaint
 			);
-			
-		}else if(handler == 4){
+
+		} else if(handler == 4){
 			// Куб (ArrowHead)
 			Handles.color = Handles.zAxisColor;
 			Handles.CubeHandleCap(
 				0,
-				t.position + Vector3.forward*1.2f,
+				t.position + Vector3.forward * 1.2f,
 				t.rotation * Quaternion.LookRotation(Vector3.forward),
 				0.3f,
 				EventType.Repaint
 			);
-			
-		}else if(handler == 5){
+
+		} else if(handler == 5){
 			// Цилиндр (ArrowHead)
 			Handles.color = Handles.zAxisColor;
 			Handles.CylinderHandleCap(
 				0,
-				t.position + Vector3.forward*1.2f,
+				t.position + Vector3.forward * 1.2f,
 				t.rotation * Quaternion.LookRotation(Vector3.forward),
 				0.3f,
 				EventType.Repaint
 			);
-			
-		}else if(handler == 6){
+
+		} else if(handler == 6){
 			// Make a 3D disc that can be dragged with the mouse
 			// Меняет поворот по оси Vector3.up
 			t.rotation = Handles.Disc(t.rotation, t.transform.position, Vector3.up, 2, false, 1);
 
-		}else if(handler == 7){
+		} else if(handler == 7){
 			// Рисует точку (Billboard Square)
 			Handles.color = Color.yellow;
 			Handles.DotHandleCap(
 				0,
-				t.position + Vector3.forward*1.2f,
+				t.position + Vector3.forward * 1.2f,
 				t.rotation * Quaternion.LookRotation(Vector3.forward),
 				0.03f,
 				EventType.Repaint
 			);
-			
-		}else if(handler == 8){
+
+		} else if(handler == 8){
 			//Draw anti-aliased convex polygon specified with point array.
 			var arrowHead = new Vector3[3];
 			var arrowLine = new Vector3[2];
 
 			Transform start = t;
 			Transform end = clv.second;
-			if (!start || !end)
+			if(!start || !end)
 				return;
 
 			var forward = (end.position - start.position).normalized;
@@ -125,8 +139,8 @@ public class CleverCubeEditor : Editor{
 			var height = size * 0.3f;
 
 			arrowHead[0] = end.position;
-			arrowHead[1] = end.position - forward * height + right*width;
-			arrowHead[2] = end.position - forward * height - right*width;
+			arrowHead[1] = end.position - forward * height + right * width;
+			arrowHead[2] = end.position - forward * height - right * width;
 
 			arrowLine[0] = start.position;
 			arrowLine[1] = end.position - forward * height;
@@ -134,8 +148,8 @@ public class CleverCubeEditor : Editor{
 			Handles.color = Color.red;
 			Handles.DrawAAPolyLine(arrowLine);
 			Handles.DrawAAConvexPolygon(arrowHead);
-			
-		}else if(handler == 9){
+
+		} else if(handler == 9){
 
 			// Эта штука не рендерится, или я не вижу или не работает.
 			float width = HandleUtility.GetHandleSize(Vector3.zero) * 0.1f;
@@ -146,41 +160,54 @@ public class CleverCubeEditor : Editor{
 				Color.red,
 				null,
 				width);
-			
-		}else if(handler == 10){
-			
+
+		} else if(handler == 10){
+
 			Handles.DrawDottedLine(t.position, clv.second.transform.position, 20f);
-			
-			
+
+
 			//  DrawDottedLines	
 			//	DrawLine	
 			//	DrawLines	
 			//	DrawPolyLine
-			
-		}else if(handler == 11){
-			
+
+		} else if(handler == 11){
+
 			Handles.DrawSolidArc(t.position,
 				t.up,
 				-t.right,
 				180,
 				clv.areaRadius);
-			
-			
+
+
 			Handles.color = Color.white;
 			clv.areaRadius =
 				Handles.ScaleValueHandle(clv.areaRadius,
 					t.position + t.forward * clv.areaRadius,
 					t.rotation,
 					1,
-					Handles.ConeCap,
+					Handles.ConeHandleCap,
 					1);
 			
-		}else if(handler == 12){
+			// много всякого не особо показательного, поэтому дальше буду реализовывать только интересные.
+			//https://docs.unity3d.com/ScriptReference/Handles.html
+
+		} else if(handler == 12){
+
+			clv.second.position = Handles.FreeMoveHandle(clv.second.position, Quaternion.identity, 0.05f, Vector3.zero, Handles.DotHandleCap);
+			Handles.DrawLine(t.position, clv.second.transform.position);
+
+		} else if(handler == 13){
+		} else if(handler == 14){
+		} else if(handler == 15){
+		} else if(handler == 16){
+		} else if(handler == 17){
+		} else if(handler == 18){
+		} else if(handler == 19){
+		} else if(handler == 20){
+		} else if(handler == 21){
+		} else if(handler == 22){
 		}
-
-
-
-
 	}
 
 	public override void OnInspectorGUI(){
