4df6489c82c4794ae6460c1e9cd0cf0f8be53d43
Nobinator <nobinator.root@gmail.com>
2017-08-01 19:45:00 +0300

    Gizmos implementation in Edtor instead of MonoBehaviour

--- Assets/Gizmos/Gizmosable.cs
+++ Assets/Gizmos/Gizmosable.cs
@@ -4,17 +4,7 @@ using UnityEngine;
 
 public class Gizmosable : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-	// Рисует всегда
+	/*// Рисует всегда
 	private void OnDrawGizmos(){
 		Gizmos.color = Color.magenta *0.2f;
 		Gizmos.DrawCube(transform.position,transform.localScale);
@@ -24,5 +14,6 @@ public class Gizmosable : MonoBehaviour {
 	private void OnDrawGizmosSelected(){
 		Gizmos.color = Color.green;
 		Gizmos.DrawWireCube(transform.position,transform.localScale);
-	}
+	}*/
+	
 }
---
+++ Assets/Gizmos/GizmosableEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+[CustomEditor(typeof(Gizmosable))]
+public class GizmosableEditor : Editor {
+
+	//Реализация Gizmos в Editor вместо MonoBehavior
+	[DrawGizmo(GizmoType.NonSelected)]
+	static void DrawGizmosActive(Gizmosable g, GizmoType gizmoType){
+		Gizmos.color = Color.magenta *0.2f;
+		Gizmos.DrawCube(g.transform.position,g.transform.localScale);
+	}
+	
+	[DrawGizmo(GizmoType.InSelectionHierarchy)]
+	static void DrawGizmos(Gizmosable g, GizmoType gizmoType){
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireCube(g.transform.position,g.transform.localScale);
+	}
+	
+}
