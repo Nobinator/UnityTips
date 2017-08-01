6cf244abab54b8f1b4acc1df7f605109e28565ec
Nobinator <nobinator.root@gmail.com>
2017-08-01 16:01:34 +0300

    AnimationCurve with Handles bezier, MakeBezierPoints(...)

--- Assets/Handles/Bezier.cs
+++ Assets/Handles/Bezier.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class Bezier : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+	public AnimationCurve curve = AnimationCurve.Linear(0, 0, 1, 0);
+
+	public float tanmultiplier;
+
 }
--- Assets/Handles/BezierEditor.cs
+++ Assets/Handles/BezierEditor.cs
@@ -2,15 +2,20 @@
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+
 [CustomEditor(typeof(Bezier))]
 public class BezierEditor : Editor{
+	private Bezier b;
+
 	private Vector3 startPos = Vector3.zero;
 	private Vector3 endPos = Vector3.zero;
 	private Vector3 startTan = Vector3.up;
 	private Vector3 endTan = Vector3.up;
 
-	private const float capSize = .07f;
-	
+	private float tanmultiplier;
+
+	private const float capSize = .03f;
+
 	private void OnEnable(){
 		Tools.current = Tool.None;
 	}
@@ -18,9 +23,12 @@ public class BezierEditor : Editor{
 
 	private void OnSceneGUI(){
 
-		var t = ((Bezier)target).gameObject.transform;
 
-		Handles.color = Color.blue;
+
+		b = ((Bezier) target);
+		var t = b.gameObject.transform;
+
+		/*Handles.color = Color.blue;
 		startTan = Handles.PositionHandle(startTan, Quaternion.identity);
 		Handles.SphereHandleCap(0,startTan,Quaternion.identity,capSize,EventType.Repaint);
 		
@@ -30,17 +38,59 @@ public class BezierEditor : Editor{
 		Handles.color = Color.magenta;
 		startPos = Handles.FreeMoveHandle(startPos, Quaternion.identity, .1f, Vector3.zero, Handles.SphereHandleCap);
 		endPos = Handles.FreeMoveHandle(endPos, Quaternion.identity, .1f, Vector3.zero, Handles.SphereHandleCap);
-		
-		
-		Handles.DrawBezier(
+		*/
+
+		for (int i = 0; i < b.curve.keys.Length - 1; i++){
+
+			var st = new Vector3(b.curve.keys[i].time, b.curve.keys[i].value, 0);
+			var en = new Vector3(b.curve.keys[i + 1].time, b.curve.keys[i + 1].value, 0);
+
+			Handles.color = Color.white;
+			mark(st, "S", i);
+			mark(en, "E", i);
+			// через curve углы как то неправильно считаются, не знаю что с этим делать
+			var stan = st + Quaternion.AngleAxis(Mathf.Rad2Deg * b.curve.keys[i].outTangent, Vector3.forward) * Vector3.right*b.tanmultiplier;
+			var etan = en + Quaternion.AngleAxis(Mathf.Rad2Deg * b.curve.keys[i + 1].inTangent, Vector3.forward) * Vector3.left*b.tanmultiplier;
+			
+			Handles.DrawLine(st,stan);
+			
+			Handles.color = Color.red;
+			mark(stan, "OutTan", i);
+			Handles.color = Color.blue;
+			mark(etan, "InTan", i);
+
+
+			Handles.DrawBezier(st, en, stan, etan, Color.white, null, 1f);
+
+			// Делает из Bezier массив точек.
+			foreach(var v in Handles.MakeBezierPoints(st, en, stan, etan, 20)){
+				mark(v,"",0);
+			}
+			
+		}
+
+		/*Handles.DrawBezier(
 			startPos,
 			endPos,
 			startTan,
 			endTan,
 			Color.white,
 			null,
-			1f);
+			1f);*/
+	}
+
+	void mark(Vector3 p,string text, int i){
+		Handles.SphereHandleCap(0, p, Quaternion.identity, capSize, EventType.Repaint);
+		Handles.Label(p + Vector3.up*0.2f,string.Format(text+" [{0}]",i),EditorStyles.largeLabel);
+			
+	}
+
+
+	void OnGUI(){
+		b.curve = EditorGUI.CurveField(
+			new Rect(3, 3, 100, 100),
+			"Animation on X", b.curve);
 		
 	}
-	
+
 }
