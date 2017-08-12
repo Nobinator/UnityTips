c68f16e01600de69b55a1c98efa47ec003093548
Nobinator <nobinator.root@gmail.com>
2017-08-12 15:56:34 +0300

    Each tip folder as .unitypackage

---
+++ UnityPackages/MovableObject.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovableObject : MonoBehaviour{
+
+	public float movingSpeed = 0.05f;
+	public float angularSpeed = 2f;
+
+	public bool angularRule = true;
+	
+	void Update () {
+		if(Input.GetKey(KeyCode.A)){
+			if(angularRule)
+				transform.Rotate(Vector3.up,-angularSpeed);
+			else
+				transform.Translate(Vector3.left * movingSpeed);
+		}
+		if(Input.GetKey(KeyCode.D)){
+			if(angularRule)
+				transform.Rotate(Vector3.up,angularSpeed);
+			else
+				transform.Translate(Vector3.right * movingSpeed);
+		}
+		if(Input.GetKey(KeyCode.W)){
+			transform.Translate(Vector3.forward * movingSpeed);
+		}
+		if(Input.GetKey(KeyCode.S)){
+			transform.Translate(Vector3.back * movingSpeed);
+		}
+	}
+}
